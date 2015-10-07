b089ca5b910ef291529be698abb5b96bd15bc45d
jerchry <544316607@qq.com>
2015-10-07 17:18:52 +0800

    My First Commit

---
+++ app/src/main/java/com/example/administrator/oschina/AppManager.java
@@ -0,0 +1,87 @@
+package com.example.administrator.oschina;
+
+import android.app.Activity;
+import android.content.Context;
+
+import java.util.Stack;
+
+/**
+ * Created by Administrator on 2015/10/7.
+ */
+public class AppManager {
+    private static Stack<Activity> activityStack;
+    private static AppManager instance;
+
+    private AppManager() {
+    }
+
+    public static AppManager getAppManager() {
+        if (instance == null) {
+            instance = new AppManager();
+        }
+        return instance;
+    }
+
+    public void addActivity(Activity activity) {
+        if (activityStack == null) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.add(activity);
+    }
+
+    public Activity currentActivity() {
+        Activity activity = activityStack.lastElement();
+        return activity;
+    }
+
+    public void finishActivity() {
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
+
+    public void finishActivity(Activity activity) {
+        if (activity != null && !activity.isFinishing()) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+    public void finishActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+                break;
+            }
+        }
+    }
+
+    public void finishAllActivity() {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                finishActivity(activityStack.get(i));
+                break;
+            }
+        }
+        activityStack.clear();
+    }
+
+    public static Activity getActivity(Class<?> cls) {
+        if (activityStack != null)
+            for (Activity activity : activityStack) {
+                if (activity.getClass().equals(cls)) {
+                    return activity;
+                }
+            }
+        return null;
+    }
+
+    public void AppExit(Context context) {
+        try {
+            finishAllActivity();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
+        } catch (Exception e) {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/administrator/oschina/AppStart.java
@@ -0,0 +1,17 @@
+package com.example.administrator.oschina;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class AppStart extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
